49dc3078f657f23c55da67318084dc97098e7e0f
Hassan <hassan_ilyas0@yahoo.com>
2020-01-14 12:08:00 +0500

    Blade views added

--- app/views/admin/sample.php
+++
@@ -1,5 +0,0 @@
-<h2>sample admin page</h2>
-<?php 
-echo do_shortcode( '[short_code]' ); 
-$table->display();
-?>
--- app/views/admin/shortcodes/sample.php
+++
@@ -1,9 +0,0 @@
-<h3>sample shortcode example</h3>
-<p><?php echo $sample; ?></p>
-
-<form action="<?php echo admin_url('admin-ajax.php'); ?>" method="post" data-form="ajax" data-cb="ajax_cb">
-	<div><input type="text" name="fname"></div>
-	<div><input type="text" name="lname"></div>
-	<input type="hidden" name="action" value="sample_action">
-	<?php echo submit_button(); ?>
-</form>
