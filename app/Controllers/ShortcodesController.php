a177893392f6855e3333c1665559af646ba1b316
Hassan <hassan_ilyas0@yahoo.com>
2020-02-17 17:35:52 +0500

    File added

---
+++ app/Controllers/ShortcodesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace _NAMESPACE_\App\Controllers;
+
+use _NAMESPACE_\Core\Controllers\WP_Controller as Controller;
+use _NAMESPACE_\App\Traits\WP_shortcodes_handler;
+
+/**
+ * Shortcodes
+ */
+class ShortcodesController extends Controller {
+
+	use WP_shortcodes_handler;
+
+	/**
+	 * Constructor
+	 *
+	 * Add your shortcodes in this constructor
+	 */
+	function __construct() {
+		parent::__construct();
+
+		add_shortcode( 'short_code', [$this, 'shortcode_cb'] );
+
+	}
+}
