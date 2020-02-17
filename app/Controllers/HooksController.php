312d491731e0ad44b89b39bed32521169c122c83
Hassan <hassan_ilyas0@yahoo.com>
2020-02-17 17:34:49 +0500

    File deleted

--- app/Controllers/HooksController.php
+++ app/Controllers/HooksController.php
@@ -17,6 +17,21 @@ class HooksController extends Controller {
 
 		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
 		add_action( 'admin_enqueue_scripts', [ $this, 'admin_scripts'] );
-		
+
+	    add_action('init', [$this, 'custom_rewrite_tag_rule'], 10, 0);
+		// add_filter( 'query_vars', [$this, 'rewrite_add_var'], 10, 1 );
 	}
+	function custom_rewrite_tag_rule() {
+    	global $wp_rewrite;
+
+		add_rewrite_tag( '%food%', '(.*)' );
+		add_rewrite_tag( '%variaty%', '(.*)' );
+		add_rewrite_rule( '^pages/(.*)/(.*)/(.*)/?$', 'index.php?pagename=$matches[1]&food=$matches[2]&variaty=$matches[3]', 'top' );
+  		// $wp_rewrite->page_structure = 'pages/%pagename%';
+		$wp_rewrite->flush_rules();
+    	// echo '<pre>';
+    	// print_r($wp_rewrite);
+    	// exit;
+    }
+
 }
