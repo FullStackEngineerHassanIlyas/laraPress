b7da2978cf20ac1bb7275e9aa09e562e742be71c
Hassan <hassan_ilyas0@yahoo.com>
2020-02-18 18:55:55 +0500

    Fixed | Added missing namespaces

--- core/Console/ConsoleCommands.php
+++ core/Console/ConsoleCommands.php
@@ -1,3 +1,9 @@
 <?php 
+
+namespace _NAMESPACE_\Core\Console;
+
+_NAMESPACE_\Core\Commands\ControllerCommand;
+_NAMESPACE_\Core\Commands\ModelCommand;
+
 $application->add( new ControllerCommand() );
 $application->add( new ModelCommand() );
